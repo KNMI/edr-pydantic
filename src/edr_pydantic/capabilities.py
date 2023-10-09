8a99eb14bbb7eb745c26fef8b77c9248abfd5a5d
Rosina Derks <rosina_derks@hotmail.com>
2023-10-09 14:52:50 +0200

    KDP-1528 remove example in landingpagemodel

--- src/edr_pydantic/capabilities.py
+++ src/edr_pydantic/capabilities.py
@@ -3,8 +3,6 @@ from __future__ import annotations
 from typing import List
 from typing import Optional
 
-from pydantic import Field
-
 from .link import Link
 from .my_base_model import EDRBaseModel
 
@@ -28,33 +26,7 @@ class Contact(EDRBaseModel):
 
 
 class LandingPageModel(EDRBaseModel):
-    links: List[Link] = Field(
-        ...,
-        # TODO Do we want this example?
-        example=[
-            {
-                "href": "http://www.example.org/edr",
-                "hreflang": "en",
-                "rel": "service-desc",
-                "type": "application/vnd.oai.openapi+json;version=3.0",
-                "title": "",
-            },
-            {
-                "href": "http://www.example.org/edr/conformance",
-                "hreflang": "en",
-                "rel": "data",
-                "type": "application/json",
-                "title": "",
-            },
-            {
-                "href": "http://www.example.org/edr/collections",
-                "hreflang": "en",
-                "rel": "data",
-                "type": "application/json",
-                "title": "",
-            },
-        ],
-    )
+    links: List[Link]
     title: Optional[str] = None
     description: Optional[str] = None
     keywords: Optional[List[str]] = None

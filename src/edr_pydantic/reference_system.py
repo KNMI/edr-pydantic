79536903e4512dfdd73918b4d8d87299462ac8d5
Rosina Derks <rosina_derks@hotmail.com>
2023-10-12 16:10:03 +0200

    Remove reference system CRS object since it is not required in the spec

--- src/edr_pydantic/reference_system.py
+++
@@ -1,9 +0,0 @@
-from .base_model import EdrBaseModel
-
-
-# TODO According to req A.13 a collection has a CRS object, according to C.1 it is a string array
-# For now, this is unused
-class CRS(EdrBaseModel):
-    name: str  # This MAY be an EPSG code
-    # TODO: Implement WKT (Well Known Text) validator
-    wkt: str

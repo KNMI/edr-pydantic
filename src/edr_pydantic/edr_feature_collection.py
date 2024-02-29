6d046efcc54ef27c52819c865dbb19ba43a65d18
Jeffrey Vervoort <jeffrey.vervoort@knmi.nl>
2024-02-29 14:23:05 +0100

    Add EDR Feature Collection to implement the optional properties specified in https://schemas.opengis.net/ogcapi/edr/1.1/openapi/schemas/edr-geojson/edrFeatureCollectionGeoJSON.yaml to help with discovery.

---
+++ src/edr_pydantic/edr_feature_collection.py
@@ -0,0 +1,9 @@
+from typing import Dict
+
+from edr_pydantic.base_model import EdrBaseModel
+from edr_pydantic.parameter import Parameter
+from geojson_pydantic import FeatureCollection
+
+
+class EDRFeatureCollection(EdrBaseModel, FeatureCollection):
+    parameters: Dict[str, Parameter]

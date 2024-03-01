0a961b2e4103106b5f0f1e501306c07456887701
Jeffrey Vervoort <jeffrey.vervoort@knmi.nl>
2024-03-01 09:45:55 +0100

    Ignore mypy error

--- src/edr_pydantic/edr_feature_collection.py
+++ src/edr_pydantic/edr_feature_collection.py
@@ -2,7 +2,7 @@ from typing import Dict
 
 from edr_pydantic.base_model import EdrBaseModel
 from edr_pydantic.parameter import Parameter
-from geojson_pydantic import FeatureCollection
+from geojson_pydantic import FeatureCollection  # type: ignore
 
 
 class EDRFeatureCollection(EdrBaseModel, FeatureCollection):
